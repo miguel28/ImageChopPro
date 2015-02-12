4f59b0b905307632bd275405b26a3292f2f3d552
miguel28 <miguelinho_28@hotmail.com>
2015-02-11 20:50:40 -0700

    Frist Commit

---
+++ ImageCropPro/frmMainWindow.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace ImageChopPro
+{
+    partial class frmMainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelectImage = new System.Windows.Forms.Button();
+            this.numWidth = new System.Windows.Forms.NumericUpDown();
+            this.numHeight = new System.Windows.Forms.NumericUpDown();
+            this.lblWidth = new System.Windows.Forms.Label();
+            this.lblHeight = new System.Windows.Forms.Label();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numWidth)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numHeight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSelectImage
+            // 
+            this.btnSelectImage.Location = new System.Drawing.Point(13, 13);
+            this.btnSelectImage.Name = "btnSelectImage";
+            this.btnSelectImage.Size = new System.Drawing.Size(75, 38);
+            this.btnSelectImage.TabIndex = 0;
+            this.btnSelectImage.Text = "Select Image";
+            this.btnSelectImage.UseVisualStyleBackColor = true;
+            this.btnSelectImage.Click += new System.EventHandler(this.btnSelectImage_Click);
+            // 
+            // numWidth
+            // 
+            this.numWidth.Location = new System.Drawing.Point(64, 61);
+            this.numWidth.Maximum = new decimal(new int[] {
+            1024,
+            0,
+            0,
+            0});
+            this.numWidth.Name = "numWidth";
+            this.numWidth.Size = new System.Drawing.Size(120, 20);
+            this.numWidth.TabIndex = 1;
+            this.numWidth.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // numHeight
+            // 
+            this.numHeight.Location = new System.Drawing.Point(64, 87);
+            this.numHeight.Maximum = new decimal(new int[] {
+            1024,
+            0,
+            0,
+            0});
+            this.numHeight.Name = "numHeight";
+            this.numHeight.Size = new System.Drawing.Size(120, 20);
+            this.numHeight.TabIndex = 2;
+            this.numHeight.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // lblWidth
+            // 
+            this.lblWidth.AutoSize = true;
+            this.lblWidth.Location = new System.Drawing.Point(10, 63);
+            this.lblWidth.Name = "lblWidth";
+            this.lblWidth.Size = new System.Drawing.Size(35, 13);
+            this.lblWidth.TabIndex = 3;
+            this.lblWidth.Text = "Width";
+            // 
+            // lblHeight
+            // 
+            this.lblHeight.AutoSize = true;
+            this.lblHeight.Location = new System.Drawing.Point(7, 87);
+            this.lblHeight.Name = "lblHeight";
+            this.lblHeight.Size = new System.Drawing.Size(38, 13);
+            this.lblHeight.TabIndex = 4;
+            this.lblHeight.Text = "Height";
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(13, 113);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox.TabIndex = 5;
+            this.pictureBox.TabStop = false;
+            // 
+            // frmMainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 392);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.lblHeight);
+            this.Controls.Add(this.lblWidth);
+            this.Controls.Add(this.numHeight);
+            this.Controls.Add(this.numWidth);
+            this.Controls.Add(this.btnSelectImage);
+            this.Name = "frmMainWindow";
+            this.Text = "Image Chop Pro";
+            ((System.ComponentModel.ISupportInitialize)(this.numWidth)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numHeight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelectImage;
+        private System.Windows.Forms.NumericUpDown numWidth;
+        private System.Windows.Forms.NumericUpDown numHeight;
+        private System.Windows.Forms.Label lblWidth;
+        private System.Windows.Forms.Label lblHeight;
+        private System.Windows.Forms.PictureBox pictureBox;
+    }
+}
+
---
+++ ImageCropPro/frmMainWindow.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+namespace ImageChopPro
+{
+    public partial class frmMainWindow : Form
+    {
+        public frmMainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Multiselect = true;
+            DialogResult result = dialog.ShowDialog();
+            if(result == DialogResult.OK)
+            {
+                foreach (String file in dialog.FileNames)
+                    convertImage(file);
+            }
+        }
+
+        public void convertImage(string file)
+        {
+            Bitmap img = new Bitmap(file);
+            Double factor = 0.0;
+            if (img.Width >= img.Height)
+                factor = (double)numHeight.Value / (double)img.Height;
+            else
+                factor = (double)numWidth.Value / (double)img.Width;
+
+            Size s = new Size((int)(img.Width * factor * 1.02), (int)(img.Height * factor * 1.02));
+
+            Console.WriteLine(s.ToString());
+            Bitmap resized = resizeImage(img, s);
+
+            pictureBox.Image = resized;
+            int offset = 0;
+            Rectangle rect;
+
+            if (img.Width >= img.Height)
+            {
+                offset = (s.Width - (int)numWidth.Value) / 2;
+                rect = new Rectangle(offset, 0, (int)numWidth.Value, (int)numHeight.Value);
+            }
+            else
+            {
+                offset = (s.Height - (int)numHeight.Value) / 2;
+                rect = new Rectangle(0, offset, (int)numWidth.Value, (int)numHeight.Value);
+            }
+            Console.WriteLine(rect.ToString());
+            Bitmap finished = cropImage(resized, rect);
+            pictureBox.Image = finished;
+
+            string extension = Path.GetExtension(file);
+            string filename = Path.GetFileName(file);
+            string filenameNoExtension = Path.GetFileNameWithoutExtension(file);
+            string root = Path.GetDirectoryName(file);
+
+            string finalPath = root + "\\" + filenameNoExtension + numWidth.Value.ToString() + "x" + numHeight.Value.ToString() + ".jpg";
+            Console.WriteLine(finalPath);
+            finished.Save(finalPath, System.Drawing.Imaging.ImageFormat.Jpeg);
+        }
+
+        public Bitmap resizeImage(Image imgToResize, Size size)
+        {
+           return new Bitmap(imgToResize, size);
+        }
+
+        private Bitmap cropImage(Bitmap img, Rectangle cropArea)
+        {
+            Bitmap bmpImage = new Bitmap(img);
+            return bmpImage.Clone(cropArea, bmpImage.PixelFormat);
+        }
+    }
+}
